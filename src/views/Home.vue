2d2f042e7163f0641ee973bf81ec6cb954474da6
null <null>
2025-01-19 21:37:17 +1300

    Demo working with conditional lists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,76 @@
-<script setup lang="ts">
-import Drawer from '@/components/Drawer.vue'
-import JSONViewer from '@/components/JSONViewer.vue'
-import { useRegle } from '@regle/core'
-import { applyIf, minLength, required } from '@regle/rules'
-import { ref } from 'vue'
+<script lang="ts" setup>
+import { z } from 'zod';
+import {ref,computed} from 'vue';
+import { useZodRegle} from '@regle/zod'
+import JSONViewer from '../components/JSONViewer.vue'
+import Drawer from '../components/Drawer.vue'
 
-const condition = ref(true)
+const data = ref<
+  Partial<{
+    condition: boolean;
+    nested: {
+      list: {
+        name: string;
+      }[];
+    };
+  }>
+>({});
 
-const { r$ } = useRegle(
-  { name: '' },
-  {
-    name: {
-      required: applyIf(condition, required),
-      minLength: applyIf(condition, minLength(3)),
-    },
-  },
-)
+const schema = computed(() => {
+  if (data.value.condition === true) {
+    return z.object({
+      condition: z.boolean(),
+      nested: z.object({
+        list: z.array(
+          z.object({
+            name: z.string(),
+          }),
+        ),
+      }),
+    });
+  }
+  return z.object({
+    condition: z.boolean(),
+  });
+});
+
+const rewardEarly = ref(true);
+const { r$ } = useZodRegle(data, schema, { rewardEarly });
 </script>
 
 <template>
-  <div class="px-6 text-gray-900 antialiased">
-    <div class="mx-auto max-w-xl py-12 md:max-w-4xl">
-      <h2 class="text-2xl mb-8">Regle reproduction</h2>
-      <div class="flex flex-col">
-        <label>Your name</label>
-        <input class="border p-2 rounded" v-model="r$.$value.name" placeholder="Victor Regle" />
-        <ul class="text-red-400 text-sm" v-if="r$.$errors.name.length">
-          <li v-for="error of r$.$errors.name" :key="error">{{ error }}</li>
-        </ul>
-      </div>
-      <Drawer>
-        <JSONViewer :data="r$" />
-      </Drawer>
+  <label>
+    <input type="radio" v-model="r$.$fields.condition.$value" :value="true" />
+    yes
+  </label>
+
+  <label>
+    <input type="radio" v-model="r$.$fields.condition.$value" :value="false" />
+    no
+  </label>
+
+  <div v-if="r$.$fields.condition.$value">
+    <div v-for="(field, ix) in r$.$fields.nested?.$fields.list.$each">
+      <input v-model="field.$fields.name.$value" :key="field.$id" />
     </div>
+
+    <button
+      @click="
+        () => {
+          const list = r$.$fields.nested?.$fields.list?.$value ?? [];
+          r$.$fields.nested.$fields.list.$value = list;
+          list.push({ name: 'john' });
+        }
+      "
+    >
+      add ({{ r$.$fields.nested?.$fields.list?.$value?.length }})
+    </button>
+  
+    <!--
+    <Drawer>
+      <JSONViewer :data="r$" />
+    </Drawer>
+    -->
   </div>
 </template>
+
